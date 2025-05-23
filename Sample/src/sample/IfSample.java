32e8becad56cfa6a1c4e6a198c5edf60972b4ee3
dir-FY25hasegawayushi <yushi.hasegawa.a723@dirbato.co.jp>
2025-05-23 14:57:51 +0900

    Lesson2 7.1時点

---
+++ Sample/src/sample/IfSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        var score = 80; // 成績
+
+        if (score >= 60) {
+            System.out.println("合格です！");
+        }
+    }
+}
+
+
+
